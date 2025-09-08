23e3561311a3f31f4556f651a53a96581bb04cd7
RambirVats51 <rambirvats1984@gmail.com>
2025-09-08 08:18:39 +0530

    backend changed

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,9 +1,9 @@
 terraform {
   backend"azurerm" {
-    resource_group_name  = "om-rg"
-    storage_account_name = "namo"
-    container_name       = "naryana"
-    key                 = "module.tfstate"
+    resource_group_name  = "om51-rg"
+    storage_account_name = "namo51"
+    container_name       = "tfstate51"
+    key                 = "module51.tfstate"
   }
     required_providers {
         azurerm= {
